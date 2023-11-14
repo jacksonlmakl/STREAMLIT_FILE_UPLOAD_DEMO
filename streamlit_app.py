d0359f687eca72f97e692188110b48624f2de3c7
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2023-11-13 19:49:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import streamlit as st
+import json
 st.header('Upload Data Files')
 uploaded_file = st.file_uploader('Upload a Parquet File')
 uploaded_json_file = st.file_uploader('Upload JSON Data Transformation Files')
 if uploaded_file != None:
   df = pd.read_parquet(uploaded_file, engine='pyarrow')
   st.write(df)
+if uploaded_json_file != None:
+  json_file = json.loads(uploaded_json_file)
+  st.write(json_file)
