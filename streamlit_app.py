2c509e3afed70e300afa44f9e8da93b6748ee189
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2023-11-13 20:02:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ if uploaded_file != None:
   df = pd.read_parquet(uploaded_file, engine='pyarrow')
   st.write(df)
 if uploaded_json_file != None:
-  json_file = json.load(uploaded_json_file.name)
+  json_file = json.load(uploaded_json_file.getbuffer())
   st.write(str(json_file))
