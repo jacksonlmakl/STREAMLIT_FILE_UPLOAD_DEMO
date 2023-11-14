b7269550da19a45768bdd12e7a9af9de34d7779d
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2023-11-13 19:19:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import streamlit as st
+st.header('Multiple File Upload')
+uploaded_files = st.file_uploader('Upload your files',
+ accept_multiple_files=True)
 
+for f in uploaded_files:
+    st.write(f)
+
+data_list = []
+for f in uploaded_files:
+    data = pd.read_csv(f)
+    data_list.append(data)
+
+for df in data_list:
+    st.write(df)
