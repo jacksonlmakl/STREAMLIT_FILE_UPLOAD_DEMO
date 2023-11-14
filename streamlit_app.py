f0a01d2dab3503017d757beb38b1f7c3ce1e71c0
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2023-11-13 21:34:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,60 +36,65 @@ st.header('Upload Data Files')
 uploaded_file = st.file_uploader('Upload a Parquet File')
 uploaded_json_file = st.file_uploader('Upload JSON Data Transformation Files')
 if uploaded_file != None:
-  df = pd.read_parquet(uploaded_file, engine='pyarrow')
-  if st.button('Create Table From Uploaded Data'):
-      table_name = st.text_input('Table Name')
-    if st.button('Execute'):
-        sql_code = pd.io.sql.get_schema(df.reset_index(), table_name)
-        cur = conn.cursor()
-        cur.execute(sql_code)
-        conn.commit()
-        cur.close()
-  st.write(df)
-  if st.button('Insert Data From Uploaded Table Into Existing SQL table '):
-    table_name = st.text_input('Table Name')
+    df = pd.read_parquet(uploaded_file, engine='pyarrow')
+    st.write(df)
+    
+    if st.button('Create Table From Uploaded Data'):
+        table_name = st.text_input('Table Name')
+        if st.button('Execute'):
+            sql_code = pd.io.sql.get_schema(df.reset_index(), table_name)
+            cur = conn.cursor()
+            cur.execute(sql_code)
+            conn.commit()
+            cur.close()
+  
+    if st.button('Insert Data From Uploaded Table Into Existing SQL table '):
+        table_name = st.text_input('Table Name')
     if st.button('Execute'):
         sql_codes = insert_statement(df_new, table_name)
         for sql_code in sql_codes:
-          cur = conn.cursor()
-          cur.execute(sql_code)
-          conn.commit()
-          cur.close()
+            cur = conn.cursor()
+            cur.execute(sql_code)
+            conn.commit()
+            cur.close()
+
+
+
 if uploaded_json_file != None:
-  json_file = json.load(uploaded_json_file)
-  
+    json_file = json.load(uploaded_json_file)
 if st.button('Transform Data'):
-  df_new = df
-  transform_cols = list(json_file.keys())
-  transformations = []
-  for i in transform_cols:
-      actions = list(json_file[i].keys())
-      for action_name in actions:
-          action = json_file[i][action_name]
-          transformations.append((i,action_name,action))
-  for transformation in transformations:
-      col_name = transformation[0]
-      pd_method = transformation[1]
-      pd_method_value = transformation[2]
-      eval_str = f"df_new['{col_name}'].{pd_method}({pd_method_value})"
-      df_new[col_name] = eval(eval_str)
-  st.write(df_new)
-  if st.button('Create Table From Transformed Table'):
-    table_name = st.text_input('Table Name')
+    df_new = df
+    transform_cols = list(json_file.keys())
+    transformations = []
+    for i in transform_cols:
+        actions = list(json_file[i].keys())
+    for action_name in actions:
+        action = json_file[i][action_name]
+        transformations.append((i,action_name,action))
+    for transformation in transformations:
+        col_name = transformation[0]
+        pd_method = transformation[1]
+        pd_method_value = transformation[2]
+        eval_str = f"df_new['{col_name}'].{pd_method}({pd_method_value})"
+        df_new[col_name] = eval(eval_str)
+    st.write(df_new)
+    
+    if st.button('Create Table From Transformed Table'):
+        table_name = st.text_input('Table Name')
     if st.button('Execute'):
         sql_code = pd.io.sql.get_schema(df_new.reset_index(), table_name)
         cur = conn.cursor()
         cur.execute(sql_code)
         conn.commit()
         cur.close()
-  if st.button('Insert Data From Transformed Table Into Existing SQL table '):
-    table_name = st.text_input('Table Name')
-    if st.button('Execute'):
-        sql_codes = insert_statement(df_new, table_name)
+    if st.button('Insert Data From Transformed Table Into Existing SQL table '):
+        table_name = st.text_input('Table Name')
+        if st.button('Execute'):
+            sql_codes = insert_statement(df_new, table_name)
         for sql_code in sql_codes:
-          cur = conn.cursor()
-          cur.execute(sql_code)
-          conn.commit()
-          cur.close()
+            cur = conn.cursor()
+            cur.execute(sql_code)
+            conn.commit()
+            cur.close()
     
   
